c5869759b990610c3a3bad542d1beafd33f00f19
steve dekerf <steve.dekerf@hotmail.com>
2017-05-18 22:07:16 +0200

    gs

---
+++ workers/ProgressWorker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SyncWPF.workers
+{
+    class ProgressWorker
+    {
+
+        public static ProgressBar initProgress(double max, bool indeterminate, Brush color)
+        {
+            var pbar = new ProgressBar();
+            pbar.Maximum = max;
+            pbar.IsIndeterminate = indeterminate;
+            pbar.BorderBrush = color;
+
+            return pbar;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
